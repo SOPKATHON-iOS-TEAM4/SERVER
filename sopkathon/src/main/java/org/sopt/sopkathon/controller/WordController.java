c65a945850efd88625423b3e6fada085d505be0b
Willy <willy_1020@naver.com>
2024-05-19 04:37:16 +0900

    [✨Feat/#16] 단어 리스트 조회 API 구현

--- sopkathon/src/main/java/org/sopt/sopkathon/controller/WordController.java
+++ sopkathon/src/main/java/org/sopt/sopkathon/controller/WordController.java
@@ -1,5 +1,6 @@
 package org.sopt.sopkathon.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.sopt.sopkathon.dto.request.WordRequest;
 import org.sopt.sopkathon.exception.dto.SuccessResponse;
@@ -15,6 +16,7 @@ public class WordController {
     private final WordService wordService;
 
     @PostMapping("/{category_id}/words/input")
+    @Operation(summary = "새로운 단어 추가 API", description = "새로운 단어 추가 API 구현")
     public ResponseEntity<SuccessResponse> addWord(
             @PathVariable("category_id") Long category_id,
             @RequestBody WordRequest wordRequest) {
@@ -23,6 +25,7 @@ public class WordController {
     }
 
     @GetMapping("/{category_id}/words")
+    @Operation(summary = "단어 리스트 조회 API", description = "단어 리스트 조회 API 구현")
     public ResponseEntity<SuccessResponse> getWords(
             @PathVariable("category_id") Long category_id) {
                 SuccessResponse response = wordService.getWords(category_id);
